e7e65b7d6597cbac39a6997915ccb1917d5436b7
Mahmudul Hasan Rifat <mahmudulhasanrifat220@gmail.com>
2024-11-12 22:54:39 +0600

    React Router Warning Sovled

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -32,4 +32,15 @@ export const router = createBrowserRouter([
             },
         ]
     },
-]);
+],
+{
+    future: {
+    v7_startTransition: true,
+    v7_skipActionErrorRevalidation: true,
+    v7_fetcherPersist: true,
+    v7_normalizeFormMethod: true,
+    v7_partialHydration: true,
+    v7_relativeSplatPath: true,
+    },
+    }
+);
