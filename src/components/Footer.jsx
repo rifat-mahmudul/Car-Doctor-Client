55f79f283ae287e074427cd2e567152545634437
Mahmudul Hasan Rifat <mahmudulhasanrifat220@gmail.com>
2024-11-12 21:25:57 +0600

    Footer Done

---
+++ src/components/Footer.jsx
@@ -0,0 +1,41 @@
+import logo from "../assets/logo.svg"
+
+
+const Footer = () => {
+    return (
+        <footer className=" bg-base-200 text-base-content">
+            <div className="footer max-w-[90%] xl:max-w-[1200px] mx-auto py-10">
+                <aside>
+                    <img src={logo} alt="" />
+                    <p>
+                    ACME Industries Ltd.
+                    <br />
+                    Providing reliable tech since 1992
+                    </p>
+                </aside>
+                <nav>
+                    <h6 className="footer-title">Services</h6>
+                    <a className="link link-hover">Branding</a>
+                    <a className="link link-hover">Design</a>
+                    <a className="link link-hover">Marketing</a>
+                    <a className="link link-hover">Advertisement</a>
+                </nav>
+                <nav>
+                    <h6 className="footer-title">Company</h6>
+                    <a className="link link-hover">About us</a>
+                    <a className="link link-hover">Contact</a>
+                    <a className="link link-hover">Jobs</a>
+                    <a className="link link-hover">Press kit</a>
+                </nav>
+                <nav>
+                    <h6 className="footer-title">Legal</h6>
+                    <a className="link link-hover">Terms of use</a>
+                    <a className="link link-hover">Privacy policy</a>
+                    <a className="link link-hover">Cookie policy</a>
+                </nav>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
