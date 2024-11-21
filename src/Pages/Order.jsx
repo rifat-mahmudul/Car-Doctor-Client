f29aa551087df592e70cb8e6ba2be2ab1bbe96e4
Mahmudul Hasan Rifat <mahmudulhasanrifat220@gmail.com>
2024-11-21 20:08:42 +0600

    Table Titile Changed

--- src/Pages/Order.jsx
+++ src/Pages/Order.jsx
@@ -21,8 +21,8 @@ const Order = () => {
                 <tr>
                     <th></th>
                     <th>Name</th>
-                    <th>Job</th>
-                    <th>Favorite Color</th>
+                    <th>Price</th>
+                    <th>Status</th>
                     <th></th>
                 </tr>
             </thead>
